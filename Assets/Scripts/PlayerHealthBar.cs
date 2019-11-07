69bb1e4d4061437dcfc6824cdb5c319bd74b3bce
Joanna <joannakwh@gmail.com>
2019-11-07 00:58:05 -0800

    billboarding script and start health bar fill

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Image fillImage;
+    [SerializeField]
+    public float maximumHealth;
+    public float currentHealth;
+    public float currentFill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fillImage = GetComponent<Image>();
+        fillImage.fillAmount = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
