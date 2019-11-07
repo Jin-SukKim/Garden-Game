f1125a6fd29e724ac5fdc7154d070b18530287b1
Joanna <joannakwh@gmail.com>
2019-11-07 02:36:14 -0800

    health bar script update

--- Assets/Scripts/PlayerHealthBar.cs
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -3,24 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealthBar : MonoBehaviour
-{
-    public Image fillImage;
-    [SerializeField]
-    public float maximumHealth;
-    public float currentHealth;
-    public float currentFill;
 
-    // Start is called before the first frame update
+public class PlayerHealthBar : MonoBehaviour {
+    [SerializeField]
+    public float lerpSpeed = 1;
+    private Image fillImage;
+    private float currentFill;
+    [SerializeField]
+    public float maxHealth { get; set; }
+
+    //property for the health bar's currentValue;
+    public float CurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+
+        set
+        {
+            //validate values are within range
+            if (value > CurrentValue)
+            {
+                currentValue = maxHealth;
+            } else if (value < 0) {
+                currentValue = 0;
+            } else {
+                currentValue = value;
+            }
+
+            //get current fill as a number from 0 to 1
+            currentFill = currentFill / maxHealth;
+        }
+    }
+    private float currentValue;
+
+
     void Start()
     {
-        //fillImage = GetComponent<Image>();
-        fillImage.fillAmount = 0.5f;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //lerp
+        if (currentFill != fillImage.fillAmount) {
+            fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+
+        //manual controls for testing
+        if(Input.GetKeyDown("J"))
+        {
+            CurrentValue -= 10;
+        }
+        if (Input.GetKeyDown("K"))
+        {
+            CurrentValue += 10;
+        }
     }
 }
