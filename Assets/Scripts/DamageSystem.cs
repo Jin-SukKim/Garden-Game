1c5c0081fbe2982181f7223405c5169407be05a6
Justin Kwok <jkwok51@my.bcit.ca>
2019-11-14 10:37:26 -0800

    Respawn
    Preliminary respawning without entity class. Also NavMesh updated.

--- Assets/Scripts/DamageSystem.cs
+++ Assets/Scripts/DamageSystem.cs
@@ -6,10 +6,12 @@ public class DamageSystem : MonoBehaviour
 {
     public float health;
 
-
     private float currentHealth;
+    public bool respawn = false;
 
     public GameObject enemeyDeathAnimation;
+    public GameObject respawnPoint;
+    public GameObject respawnPointWait;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +22,37 @@ public class DamageSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currentHealth <= 0)
+        if (currentHealth <= 0 && respawn == false)
         {
             GameObject obj = (GameObject)Instantiate(enemeyDeathAnimation, transform.position, Quaternion.identity);
             //Destroy(gameObject.GetComponent("Rigidbody"));
             Destroy(obj, 5f);
             Destroy(gameObject);
         }
+        else if (currentHealth <= 0 && respawn == true)
+        {
+            gameObject.transform.position = respawnPointWait.transform.position;
+            StartCoroutine(WaitingRespawn());
+        }
     }
 
     public void Damage(float damage)
     {
         currentHealth -= damage;
+    }
+
+    public void Respawn()
+    {
+        if (respawn)
+        {
+            gameObject.transform.position = respawnPoint.transform.position;
+            currentHealth = health;
+        }
+    }
+
+    IEnumerator WaitingRespawn()
+    {
+        yield return new WaitForSeconds(3);
+        Respawn();
     }
 }
