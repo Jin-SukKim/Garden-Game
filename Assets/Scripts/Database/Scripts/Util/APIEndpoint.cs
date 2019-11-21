cc2afeff1841a00bf70005ff81d264e47293ea71
Johnny Pham <johnnypham@outlook.com>
2019-11-21 11:37:05 -0800

    added string, fix for develop buiild errors

--- Assets/Scripts/Database/Scripts/Util/APIEndpoint.cs
+++ Assets/Scripts/Database/Scripts/Util/APIEndpoint.cs
@@ -8,5 +8,6 @@ public class APIEndpoint : MonoBehaviour {
     public static string CreateAccountCloud = "https://techpro2019.azurewebsites.net/api/account/create";
     public static string LoginLocal = "localhost:54882/api/login/{0}/{1}";
     public static string LoginCloud = "https://techpro2019.azurewebsites.net/api/login/{0/{1}}";
+    public static string UpdateStatsLocal = "https://techpro2019.azurewebsites.net/api/login/{0/{1}}";
 
 }
