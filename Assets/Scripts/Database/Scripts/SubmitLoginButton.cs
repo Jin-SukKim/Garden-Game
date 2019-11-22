ba71cea1a0e2a311d2b462c2fb99f685ba58c3c6
marvin <marvinbtngo@gmail.com>
2019-11-21 17:05:41 -0800

    wip 2

--- Assets/Scripts/Database/Scripts/SubmitLoginButton.cs
+++ Assets/Scripts/Database/Scripts/SubmitLoginButton.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SubmitLoginButton : MonoBehaviour {
 
@@ -16,14 +17,9 @@ public class SubmitLoginButton : MonoBehaviour {
     [SerializeField]
     private InputField PasswordInputField;
 
-    [SerializeField]
-    private InputField PlayerNameInputField;
-
     void Start() {
         Button loginButton = gameObject.GetComponent<Button>();
         StatusText = GameObject.Find("statusText").GetComponent<Text>();
-        UsernameInputField = GameObject.Find("UsernameInputField").GetComponent<InputField>();
-        PasswordInputField = GameObject.Find("PasswordInputField").GetComponent<InputField>();
         loginButton.onClick.AddListener(LoginButtonClick);
     }
 
@@ -33,14 +29,16 @@ public class SubmitLoginButton : MonoBehaviour {
         string username = UsernameInputField.text;
         string passwordHash = PasswordHash.Generate(PasswordInputField.text);
 
-        string localUrl = string.Format(APIEndpoint.LoginLocal, username, passwordHash);
-        string cloudUrl = string.Format(APIEndpoint.LoginCloud, UsernameInputField.text);
+        //string localUrl = string.Format(APIEndpoint.LoginLocal, username, passwordHash);
+        //string cloudUrl = string.Format(APIEndpoint.LoginCloud, UsernameInputField.text);
 
-        StartCoroutine(SendHttpRequest(localUrl, username, passwordHash));
+        //StartCoroutine(SendHttpRequest(localUrl, username, passwordHash));
+        StartCoroutine(SendHttpRequest("game", username, passwordHash));
         //StartCoroutine(SendHttpRequest(cloudUrl));
     }
 
     IEnumerator SendHttpRequest(string url, string username, string passwordHash) {
+
         WWWForm postData = new WWWForm();
         postData.AddField("username", username);
         postData.AddField("passwordHash", passwordHash);
@@ -50,24 +48,28 @@ public class SubmitLoginButton : MonoBehaviour {
 
         yield return request.SendWebRequest();
 
-        if (request.isNetworkError || request.isHttpError) {
+        if (request.isNetworkError || request.isHttpError)
+        {
             Debug.Log("error:");
             Debug.Log(request.error);
             StatusText.text = "incorrect username or password";
-        } else {
+        }
+        else
+        {
             Account account = JsonUtility.FromJson<Account>(request.downloadHandler.text);
 
-            if (PasswordHash.Validate(PasswordInputField.text, passwordHash)) {
+            if (PasswordHash.Validate(PasswordInputField.text, passwordHash))
+            {
+                PlayerPrefs.DeleteKey("guest");
                 PlayerPrefs.SetString("username", account.username);
                 PlayerPrefs.SetString("token", account.jsonWebToken);
-
                 StatusText.text = $"welcome {PlayerPrefs.GetString("username")}\n" +
                                   $"your jwt is {PlayerPrefs.GetString("token")}";
 
-                PlayerNameInputField.text = PlayerPrefs.GetString("username");
                 UsernameInputField.text = string.Empty;
                 PasswordInputField.text = string.Empty;
                 LoginButton.SetActive(false);
+                SceneManager.LoadScene("Lobby");
             }
         }
     }
