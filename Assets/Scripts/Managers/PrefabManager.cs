bd1ae86cff48c1c636c1952561374b9d8961fc25
Adam <adamkdevelop@gmail.com>
2019-11-28 15:12:36 -0800

    working on plant placement

--- Assets/Scripts/Managers/PrefabManager.cs
+++ Assets/Scripts/Managers/PrefabManager.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public static class PrefabManager
 {
     static Dictionary<string, GameObject> bullets = new Dictionary<string, GameObject>();
-    
+
+    static Dictionary<string, GameObject> placeables = new Dictionary<string, GameObject>();
+
     // static constructor for any initialization
     static PrefabManager()
     {
@@ -26,26 +28,37 @@ public static class PrefabManager
         }
     }
 
-/*    public static bool SpawnBullet(string bulletID, Vector3 pos, Quaternion rot, Entity e)
+    // loads all the placeables from the placeables folder in the resources folder
+    public static void LoadPlaceables()
     {
+        GameObject[] allBullets = Resources.LoadAll("Prefabs/Placeables", typeof(GameObject)).Cast<GameObject>().ToArray();
 
-        if (bullets.ContainsKey(bulletID))
+        // Store all bullets in the dictionary with name as key
+        for (int i = 0; i < allBullets.Length; i++)
         {
-*//*            Quaternion dir = Quaternion.LookRotation(targetLoc);*//*
-            GameObject newObj = GameObject.Instantiate(bullets[bulletID], pos, dir);
-            newObj.transform.rotation = Quaternion.Slerp(newObj.transform.rotation, dir, 0f);
-            BulletController newBullet = newObj.GetComponent<BulletController>();
-*//*            newBullet.entity = e;*//*
-            return true;
-        } else
-        {
-            return false;
+            bullets.Add(allBullets[i].name, allBullets[i]);
         }
-    }*/
+    }
+
+    /*    public static bool SpawnBullet(string bulletID, Vector3 pos, Quaternion rot, Entity e)
+        {
+
+            if (bullets.ContainsKey(bulletID))
+            {
+    *//*            Quaternion dir = Quaternion.LookRotation(targetLoc);*//*
+                GameObject newObj = GameObject.Instantiate(bullets[bulletID], pos, dir);
+                newObj.transform.rotation = Quaternion.Slerp(newObj.transform.rotation, dir, 0f);
+                BulletController newBullet = newObj.GetComponent<BulletController>();
+    *//*            newBullet.entity = e;*//*
+                return true;
+            } else
+            {
+                return false;
+            }
+        }*/
 
     public static bool SpawnBullet(string bulletID, Vector3 pos, Vector3 targetLoc, Entity e)
     {
-
         if (bullets.ContainsKey(bulletID))
         {
             Quaternion dir = Quaternion.LookRotation(targetLoc);
@@ -63,4 +76,24 @@ public static class PrefabManager
         }
     }
 
+
+    public static bool SpawnPlaceable(string objectID, Vector3 pos, Vector3 targetLoc, Entity e)
+    {
+
+        if (placeables.ContainsKey(objectID))
+        {
+            Quaternion dir = Quaternion.LookRotation(targetLoc);
+            GameObject newObj = GameObject.Instantiate(placeables[objectID], pos, dir);
+            //BulletController newObject = newObj.GetComponent<BulletController>();
+            //newBullet.InitBullet(pos, targetLoc);
+
+            //not the best way to do this
+            //newBullet.entity = e;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
