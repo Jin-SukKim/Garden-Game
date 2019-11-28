ed4daf0f6c0db5b13012953e7e7655eab5503ecb
Thompson <messagethompson@gmail.com>
2019-11-28 09:32:24 -0800

    issue done

--- Assets/Scripts/EntityScripts/Entity.cs
+++ Assets/Scripts/EntityScripts/Entity.cs
@@ -11,10 +11,16 @@ public class Entity : MonoBehaviour
     public Abilities abilities;
     public Transform spawnPoint;
 
+    public GameObject respawnPoint;
+    public float respawnPointWait;
+
     public MeshRenderer renderer;
     public CapsuleCollider collider;
 
-    public bool inputAllowed;
+    public bool respawning;
+    public bool canMove;
+    public bool canAct;
+    public bool isPlayer;
 
     private void Start()
     {
@@ -25,5 +31,58 @@ public class Entity : MonoBehaviour
         spawnPoint = gameObject.transform.Find("spawnPoint");
         renderer = GetComponent<MeshRenderer>();
         collider = GetComponent<CapsuleCollider>();
+    }
+
+    public void DeactivatePlayer()
+    {
+        collider.enabled = false;
+        renderer.enabled = false;
+        respawning = true;
+    }
+
+    public void Despawn()
+    {
+        DeactivatePlayer();
+        //this.StartCoroutine(RespawnCoroutine(Time.time + 2f, this));
+        StartCoroutine(WaitingRespawn(Time.time + respawnPointWait));
+        //StartCoroutine(routine(Time.time));
+    }
+
+    /// <summary>
+    /// Respawn function that moves the object to the respawn point and resets health
+    /// </summary>
+    public void Respawn()
+    {
+        transform.position = respawnPoint.transform.position;
+        health.currentHealth = health.health;
+        ReactivatePlayer();
+    }
+
+    public void ReactivatePlayer()
+    {
+        collider.enabled = true;
+        renderer.enabled = true;
+        respawning = false;
+    }
+
+
+    /// <summary>
+    /// Waiting function for respawn that waits before respawning the object
+    /// </summary>
+    /// <param name="timeStamp">the given timestamp for the entity to respawn</param>
+    /// <returns></returns>
+    public IEnumerator WaitingRespawn(float timeStamp)
+    {
+        while (Time.time < timeStamp)
+        {
+            // wait for spawn timer
+            yield return null;
+
+        }
+
+        
+        Respawn();
+
+        yield return null;
     }
 }
